0e074c3daafd11b8bff0cb3549d684e813046a04
Edmund Mok <edmundmok@outlook.com>
2017-10-15 01:15:02 -0700

    Use dir api instead

--- Sialah/Sialah/View/CollectionViewCell/InnerCurrentItineraryCollectionViewCell.swift
+++ Sialah/Sialah/View/CollectionViewCell/InnerCurrentItineraryCollectionViewCell.swift
@@ -19,7 +19,7 @@ class InnerCurrentItineraryCollectionViewCell: UICollectionViewCell {
     var modelIndex: Int?
 
     @IBAction func handleDirectionsButtonPressed(_ sender: Any) {
-        UIApplication.shared.open(URL(string: "https://www.google.com/maps/search/?api=1&query=centurylink+field")!, options: [:], completionHandler: nil)
+        UIApplication.shared.open(URL(string: "https://www.google.com/maps/dir/?api=1&destination=centurylink+field")!, options: [:], completionHandler: nil)
     }
 
     @IBAction func handleTicketButtonPressed(_ sender: Any) {
