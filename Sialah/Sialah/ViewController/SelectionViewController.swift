9c40cacf7cc646875a025f8ac622c6b8b02695c2
Edmund Mok <edmundmok@outlook.com>
2017-10-15 09:30:27 -0700

    Add SelectionViewController

---
+++ Sialah/Sialah/ViewController/SelectionViewController.swift
@@ -0,0 +1,31 @@
+//
+//  SelectionViewController.swift
+//  Sialah
+//
+//  Created by Edmund Mok on 10/15/17.
+//  Copyright © 2017 Leon Mak. All rights reserved.
+//
+
+import UIKit
+
+class SelectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        initCards()
+    }
+    @IBOutlet weak var stopoverBorder: UIView!
+
+    @IBOutlet weak var layoverBorder: UIView!
+    func initCards() {
+        layoverBorder.layer.borderWidth = 5
+        layoverBorder.layer.borderColor = UIColor.flatBlueDark.cgColor
+        layoverBorder.layer.cornerRadius = 20
+
+        stopoverBorder.layer.borderWidth = 5
+        stopoverBorder.layer.borderColor = UIColor.flatBlueDark.cgColor
+        stopoverBorder.layer.cornerRadius = 20
+    }
+
+}
