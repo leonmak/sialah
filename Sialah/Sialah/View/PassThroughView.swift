f25b31acdecacaccf8eeb6b4846c8cddf2f48ba4
Edmund Mok <edmundmok@outlook.com>
2017-10-14 21:49:07 -0700

    Add PassThroughView

---
+++ Sialah/Sialah/View/PassThroughView.swift
@@ -0,0 +1,21 @@
+//
+//  PassThroughView.swift
+//  Sialah
+//
+//  Created by Edmund Mok on 10/14/17.
+//  Copyright © 2017 Leon Mak. All rights reserved.
+//
+
+import UIKit
+
+class PassThroughView: UIView {
+
+    override func point(inside point: CGPoint, with event: UIEvent?) -> Bool {
+        for subview in subviews {
+            if !subview.isHidden && subview.alpha > 0 && subview.isUserInteractionEnabled && subview.point(inside: convert(point, to: subview), with: event) {
+                return true
+            }
+        }
+        return false
+    }
+}
